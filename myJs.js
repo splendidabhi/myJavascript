2a24bd4d491832b89fb95d127529a97213d42ef7
splendidabhi <abhi08071999@gmail.com>
2023-01-15 23:06:42 +0530

    array

--- myJs.js
+++ myJs.js
@@ -1,10 +1,13 @@
 function myFunction(){
     document.getElementById("sub_header").innerHTML="good ,Javascript is awesome .";
 }
-
 let a,b,c;
 a=5;
 b=10;
 c=15;
-
 console.log(a+b+c);
+
+const myArray = ['Abhinav','Arun','Satyam','Lakshay'];
+console.log(myArray);
+myArray.push('lucky');
+console.log(myArray);
